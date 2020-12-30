0df9b7884fa9f8506281461b3c9b5c85b4f6b6f7
Desmond Awungayi <dawungayi@gmail.com>
2020-12-30 11:38:16 -0600

    Adds display for randomly generated array elements - Adds random generator for array elements: both for the array size and all the elements in the array. - Adds display and styling for these bars - lots of CSS (so thankful for flex!).     -- In the array-container template, we iterate through the entire array and display a vertical bar for each element.     -- Each vertical bar has height which is relative to the max element in the array.     -- The width of each bar and the spacing between bars is dynamically calculated based on the size of the array for a better visual. The max array size with no wrap-around issues is 300. - Modified sorting-visual component to center the collection of bars horizontally on the screen and to occupy more of the screen's width.

--- src/app/components/array-bar/array-bar.component.ts
+++ src/app/components/array-bar/array-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-array-bar',
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./array-bar.component.css']
 })
 export class ArrayBarComponent implements OnInit {
-
+  @Input() element : number = 0;  // taking from parent
+  @Input() max : number = Number.MAX_VALUE;  // taking from parent. Init to max possible value
+  @Input() numElements: number = 1;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // Dynamic classes
+  setBarStyles() {
+    // console.log(`Num elts: `);
+    
+    // set css class for each array-bar
+    let barStyles = {
+      height: `${(this.element/this.max)*100}%`,  // height is relative to the max element in array 
+      // color: 'red',
+      // width: `${(100/this.numElements)}%`, 
+      // width: '80%', 
+      width: `${50/this.numElements}rem`, 
+      margin: `0 ${10/this.numElements}rem`, // this works for a max of 200 elts on a medium screen just fine
+      backgroundColor: 'turquoise',
+      /* border: 0.5px solid black; */
+      /* border: 2px solid red; */
+      display: 'inline',
+      // minWidth: '2rem',
+      // maxWidth: '3rem'
+    }
+    return barStyles;
+
+  }
+
+  // getBarStyles() {
+  //   let barStyles = {
+
+  //   };
+  //   return barStyles;
+  // }
 
 }
--- src/app/components/array-container/array-container.component.ts
+++ src/app/components/array-container/array-container.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./array-container.component.css']
 })
 export class ArrayContainerComponent implements OnInit {
-
+  numElements: number = 0; 
+  array: number[] = []; // initialize array 
+  max: number = Number.MAX_VALUE; 
   constructor() { }
 
   ngOnInit(): void {
+    this.numElements = this.getRandomIntInclusive(20, 300);
+    // console.log(this.numElements);
+    // this.array = [65, 22, 33, 45, 12];
+    this.createArray(); // populates this.array
+    // console.log(this.array);
+    
+    this.max = Math.max(...this.array);
+  }
+
+  // from MDN reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values_inclusive
+  getRandomIntInclusive(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  }
+
+  createArray() {
+    for (let i = 0; i < this.numElements; i++) {
+      this.array[i] = this.getRandomIntInclusive(0, 100);
+    }
+  }
+
+  // makes each bar to have variable width, depending on the number of elts in the array
+  getBarContainerStyles() {
+    let styles = {
+      width : `${100/this.numElements}%`
+    }
+    return styles;
   }
 
 }
