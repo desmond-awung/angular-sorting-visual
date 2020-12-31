49ab0d3df6ce70febc9a6796c93d47e4ba417708
Desmond Awungayi <dawungayi@gmail.com>
2020-12-31 08:45:35 -0600

    Adds functionlity to select the Sort Type - Adds buttons to the to the Controls component to select what sort type to be done on the array. Buttons for five sort types are given. - Next steps: implement methods for each sort type.

--- src/app/modules/sorting/components/controls/controls.component.ts
+++ src/app/modules/sorting/components/controls/controls.component.ts
@@ -4,12 +4,43 @@ import { UserArrParamsService } from '../../services/user-arr-params.service';
 // models
 import { UserParams } from '../../models/UserParams'
 
+
+// enum SortType {
+//   SelectionSort = 1,
+//   BubbleSort,
+//   InsertionSort,
+//   MergeSort,
+//   QuickSort,
+// }
+
+class SortType {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  toString() {
+    return `SortType.${this.name}`;
+  }
+}
+
 @Component({
   selector: 'app-controls',
   templateUrl: './controls.component.html',
   styleUrls: ['./controls.component.css']
 })
 export class ControlsComponent implements OnInit {
+
+  MySortType = class {
+    static BubbleSort = new SortType('BubbleSort');
+    static InsertionSort = new SortType('InsertionSort');
+    static MergeSort = new SortType('MergeSort');
+    static SelectionSort = new SortType('SelectionSort');
+    static QuickSort = new SortType('QuickSort');
+    static None = new SortType('None');
+  }
+  // sortType: SortType = SortType.None;
   msg: UserParams = {};
   // array params entered by user
   /**
@@ -17,10 +48,7 @@ export class ControlsComponent implements OnInit {
    * arrSize: the size of the array enetered by user (defaults to -1 if not specified)
    */
   arrParams: UserParams = new UserParams();
-  /*
-  user input params
-  isRandom: boolean, arrSize: number
-  */
+  
   constructor(private paramsService: UserArrParamsService) { }
 
   ngOnInit(): void {
@@ -32,24 +60,60 @@ export class ControlsComponent implements OnInit {
   setArrParams(params: UserParams) {
     
     if (params.isRandom !== undefined) {
-      console.log("Not Random Array to be generated");
       this.arrParams.isRandom = params.isRandom;
     }
     
     if (params.arrSize !== undefined) {
-      console.log("no Array size given");
       this.arrParams.arrSize = params.arrSize;
     }
     
-    console.log("clicked to setup Arr");
     console.log(this.arrParams);
     // share this object via a service to other conponents
-    // this.initArray.emit(this.arrParams);
-    // this.paramsService.changeUserParams(this.arrParams);
-    // this.msg = "Masteeeeer"
     this.msg = this.arrParams;
-    // this.msg.isRandom = false;
     this.paramsService.changeUserParams(this.msg);
+  }
+
+  sortArray(type: SortType) {
+    console.log(type);
+    
+    switch(type.name) {
+
+    case this.MySortType.SelectionSort.name:
+    {
+        console.log("Selection Sort. Begins...");
+    
+        break;
+    }
+    case  this.MySortType.BubbleSort.name:
+    {
+      console.log("Bubble Sort. Begins...");
+    
+      break;
+    }
+    case  this.MySortType.InsertionSort.name:
+    {
+      console.log("Insertion Sort. Begins...");
+    
+      break;
+    }
+    case  this.MySortType.MergeSort.name:
+    {
+      console.log("Merge Sort. Begins...");
+    
+      break;
+    }
+    case  this.MySortType.QuickSort.name:
+    {
+      console.log("Quick Sort. Begins...");
+    
+      break;
+    }
+
+    default:
+      console.log("Invalid Sort Type passed");
+      
+  }
+
 
   }
 
